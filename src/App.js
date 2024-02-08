5828da01c0d384bde687622b52125a2b22ace5cc
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-02-08 00:47:02 -0500

    Nested Routes and Showing Error Pages with errorElement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
 //https://example.com/products/
 
 
 const router = createBrowserRouter([
-  {path:'/', element: <HomePage />},
-  {path: '/products', element: <ProductPage/>}
+  {path: '/', element: <RootLayout/>,
+  errorElement : <ErrorPage />,
+  children: [
+    {path:'/', element: <HomePage />},
+    {path: '/products', element: <ProductPage/>}
+  ],
+},
 ]);
 // const routeDefinitions = createRoutesFromElements(
 // <Route>
---
+++ src/pages/Root.js
@@ -0,0 +1,17 @@
+import {Outlet} from 'react-router-dom';
+import MainNavigation from '../component/MainNavigation';
+
+function RootLayout(){
+    return (
+        <>
+       <MainNavigation />
+       <main >
+
+       <Outlet />
+       </main>
+
+        </>
+    )
+    
+}
+export default RootLayout
