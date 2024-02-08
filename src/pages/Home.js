071ff5fc3961772ce6533fc09e6873ea90a5243a
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-02-08 01:32:08 -0500

    Navigate Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 function HomePage(){
+    const navigate = useNavigate();
+//we use this when for example want to work with timer
+    function navigateHandler(){
+    navigate('/products');
+    }
+
     return (
         <>
         <h1>My Home Page</h1>
-        <p>Go to the <Link to="/products">Products</Link>.</p>
+        <p>
+            Go to the <Link to="/products">Products</Link>.
+        </p>
+        <p>
+            <button onClick={navigateHandler}>
+                Navigate</button>
+        </p>
         </>
     )
     
